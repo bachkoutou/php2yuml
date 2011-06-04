45692c4f02ea84b83c7b72a754e5b5e8a99aa6f5
Anis BEREJEB <anis.berejeb@gmail.com>
2011-06-04 19:46:52 -0400

    added size and direction support

--- php2yuml.php
+++ php2yuml.php
@@ -1,16 +1,41 @@
 <?php
 require_once(dirname(__FILE__) . '/autoloader.php');
 require_once(dirname(__FILE__) . '/CurlClient.php');
-$help = PHP_EOL. 'Usage : php php2yuml.php -s <Folder> -o <file>' . PHP_EOL;
+$help = <<<PHP_EOL
+Usage : 
+php php2yuml.php -f <SourceDirectory> -o <file> [-s <size>] [-d <direction>]
+    -f, --from-directory : Source Directory
+    -o, --output    : Output File (a yuml image)
+    -s, --size      : A size, Default size :100;
+    -d, --direction : The Direction (Orientation) of the diagram, One
+                      From the following : 
+                        - LR (Left to Right)
+                        - TB (Top to Bottom)
+                        - RL (Right to Left)
+    -h, --help      : Displays the Help Message
+PHP_EOL;
+$help  = PHP_EOL . $help . PHP_EOL;
 for ($i= 1; $i < $_SERVER['argc'];$i++) {
     switch ($_SERVER['argv'][$i]) {
-    case '-s' :
-    case '--source' :
+    case '-f' :
+    case '--from-directory' :
         $sourceFolder = $_SERVER['argv'][$i+1];
         break;
     case '-o':
     case '--output':
         $destinationFile = $_SERVER['argv'][$i+1];
+        break;
+    case '-s':
+    case '--size':
+        $scale = (int) $_SERVER['argv'][$i+1];  
+        break;
+    case '-d':
+    case '--direction':
+        $direction = $_SERVER['argv'][$i+1];
+        if (!in_array($direction, array('LR', 'RL', 'TB'))) {
+            echo ('The direction Parameter should be one of the following: LR, RL, TB');
+            die($help);
+        }
         break;
     case '-h':
     case '--help':
@@ -19,9 +44,11 @@ for ($i= 1; $i < $_SERVER['argc'];$i++) {
     }
 }
 if (!$destinationFile) {
-        die($help);
+    echo 'The -o Parameter is mandatory' . PHP_EOL;
+    die($help);
 }
 if (!$sourceFolder) {
+    echo 'The -f Parameter is mandatory' . PHP_EOL;
         die($help);
 }
 $autoloadManager = new AutoloadManager();
@@ -75,12 +102,16 @@ foreach ($autoloadManager->parseFolders() as $class => $file) {
    $components[] = $string;
     if ($interfaces = $reflection->getInterfaces()) {
         foreach ($interfaces as $name => $interface) {
-            $components[] = '[' . $name . ']^-.-' .  $class;
+            $components[] = '[<<' . $name . '>>]^-.-' .  $class;
         }
     }
 }
 $components = array_unique(array_filter($components));
 $string = implode($components, ',');
 $client = new CurlClient();
-$client->setUrl('http://yuml.me/diagram/scruffy/class/' . rawurlencode($string));
+$direction = !empty($direction) ? 'dir:' . $direction . ';' : '';
+$scale = !empty($scale) ? 'scale:' . $scale . ';' : '';
+$params = ';' . $direction . $scale;
+$client->setUrl('http://yuml.me/diagram/scruffy' . $params . '/class/' . rawurlencode($string));
 file_put_contents($destinationFile, $client->call());
+
